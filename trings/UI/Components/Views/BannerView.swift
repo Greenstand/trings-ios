5e325f2e35555e7c03017d4c7444123c7b00224e
Rob Huber <rmh211@gmail.com>
2022-11-07 15:35:23 -0600

    AddedUI
    Added Walkthrough guides
    Added Wallet UI
    Added Purchase Token UI

---
+++ trings/UI/Components/Views/BannerView.swift
@@ -0,0 +1,8 @@
+//
+//  BannerView.swift
+//  trings
+//
+//  Created by Robert Huber on 11/3/22.
+//
+
+import Foundation
---
+++ trings/UI/Views/MyWallet/TreeCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TreeCollectionViewCell.swift
+//  trings
+//
+//  Created by Robert Huber on 11/3/22.
+//
+
+import UIKit
+
+class TreeCollectionViewCell: UICollectionViewCell {
+    
+}
