80e205824abcf33ec927d9394c1533b4979e24bf
Pedro Ésli <pedroesli.vn@gmail.com>
2023-08-18 11:25:18 -0300

    feat: created walkthrough storyboard pages and added a viewcontainer in WalktroughGuide

---
+++ trings/UI/Views/WalkthroughGuide/Pages/WalkthroughPageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WalkthroughPageViewController.swift
+//  trings
+//
+//  Created by Pedro Ésli Vieira do Nascimento on 18/08/23.
+//
+
+import UIKit
+
+class WalkthroughPageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
